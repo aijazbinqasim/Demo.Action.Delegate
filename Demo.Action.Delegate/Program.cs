4ae3917b303fbc0bf84de878563f1a64c9268ab0
Aijaz Bin Qasim <aijazbinqasim@hotmail.com>
2024-12-20 22:19:23 +0500

    Add project files.

---
+++ Demo.Action.Delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Demo.Action.Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> action = A.Add;
+            action(100, 300);
+
+            Action<string, string> action2 = A.Concat;
+            action2("Hello", "World");
+
+            Action<int> square = delegate (int x)
+            {
+                Console.WriteLine(x * x);
+            };
+            square(5);
+
+            // write action using lambda expression
+            Action<int> cube = x => Console.WriteLine(x * x * x);
+            cube(5);
+        }
+    }
+
+    public class A
+    {
+        public static void Add(int a, int b) => Console.WriteLine(a + b);
+        public static void Concat(string a, string b) => Console.WriteLine($"{a} - {b}");
+    }
+}
